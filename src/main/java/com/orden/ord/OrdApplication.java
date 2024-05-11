7e1d513a2bcd5aec14239a93eb0fe04ed95da486
ldavidtl <torres_d44@yahoo.es>
2024-05-11 14:25:29 -0600

    initial commit

---
+++ src/main/java/com/orden/ord/OrdApplication.java
@@ -0,0 +1,13 @@
+package com.orden.ord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orden/ord/OrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orden.ord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
