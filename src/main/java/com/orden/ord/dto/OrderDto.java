a066c0a73fcfa388a2b8abf34d9c81757b20ae53
ldavidtl <torres_d44@yahoo.es>
2024-05-11 22:06:18 -0600

    add dtos from order detal and fieng client

---
+++ src/main/java/com/orden/ord/dto/OrderDto.java
@@ -0,0 +1,61 @@
+package com.orden.ord.dto;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+
+import com.orden.ord.emnums.EstatusOrder;
+
+public class OrderDto {
+	
+	private Long orderId;
+
+	private Date dateOrder;
+
+	private EstatusOrder status;
+
+	private BigDecimal total;
+	
+	private List<OrderDetailDto> details;
+
+	public Long getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+
+	public Date getDateOrder() {
+		return dateOrder;
+	}
+
+	public void setDateOrder(Date dateOrder) {
+		this.dateOrder = dateOrder;
+	}
+
+	public EstatusOrder getStatus() {
+		return status;
+	}
+
+	public void setStatus(EstatusOrder status) {
+		this.status = status;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+	public List<OrderDetailDto> getDetails() {
+		return details;
+	}
+
+	public void setDetails(List<OrderDetailDto> details) {
+		this.details = details;
+	}
+	
+}
